d1cd599b393c525d3b23072f263364970f8b3b7d
Vladimir Ivanov <icu0755@gmail.com>
2017-11-21 22:48:59 +0100

    configure django to use mysql

--- mysql_timeout/mysql_timeout/settings.py
+++ mysql_timeout/mysql_timeout/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysql_timeout.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
